66b5d0022f847f01a402a7ab83be96cea63f6536
suheesh mahi <suheeshk@gmail.com>
2015-11-12 13:05:46 +0530

    Added the hangman initial file

---
+++ myhang.py
@@ -0,0 +1,26 @@
+import random
+
+def get_wordlist()
+    f=open("/usr/share/dict/words")
+    clean_words = []
+    for i in f:
+        i=i.strip()
+        if i.isalpha() and len(i)>5:
+            clean_words.append(i.lower())
+    return clean_words
+
+
+def select_word(word_list):
+    return random.sample(wordlist,1)[0]
+
+
+def play_hangman(secret_word):
+    
+    #homework
+
+
+wordlist = get_wordlist()
+secret_word = select_word(wordlist)
+play_hangman(secret_word)
+    
+    
